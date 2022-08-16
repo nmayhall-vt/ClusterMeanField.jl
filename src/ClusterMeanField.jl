bca14b14013cc7894f05d41abb9a47a05efde7cb
Nick Mayhall <nmayhall@vt.edu>
2022-08-16 12:00:34 -0400

    cmf seems to be working

--- src/ClusterMeanField.jl
+++ src/ClusterMeanField.jl
@@ -1,5 +1,24 @@
 module ClusterMeanField
 
-# Write your package code here.
+using LinearAlgebra
+using Random
+using Optim
+using TensorOperations
+using InCoreIntegrals
+using Printf
+using ActiveSpaceSolvers
+
+export Atom
+export Molecule
+export Cluster
+export form_1rdm_dressed_ints
+export cmf_ci
+export cmf_oo
+
+include("type_Atom.jl")
+include("type_Molecule.jl")
+include("type_Cluster.jl")
+include("PyscfFunctions.jl")
+include("CMFs.jl")
 
 end

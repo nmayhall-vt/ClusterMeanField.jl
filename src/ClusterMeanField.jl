de52158293e13d6efd8a26a949899ebdcac5d212
Nick Mayhall <nmayhall@vt.edu>
2022-08-16 18:07:19 -0400

    added lots of pyscf interface exports

--- src/ClusterMeanField.jl
+++ src/ClusterMeanField.jl
@@ -15,6 +15,18 @@ export form_1rdm_dressed_ints
 export cmf_ci
 export cmf_oo
 
+export pyscf_do_scf
+export make_pyscf_mole
+export pyscf_write_molden
+export pyscf_build_1e
+export pyscf_build_eri
+export pyscf_get_jk
+export pyscf_build_ints
+export pyscf_fci
+export get_nuclear_rep
+export localize
+export get_ovlp
+
 include("type_Atom.jl")
 include("type_Molecule.jl")
 include("type_Cluster.jl")

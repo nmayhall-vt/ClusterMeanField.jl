bb5411b1ae3d05b8cb0c585509c069958d523c7a
Nick Mayhall <nmayhall@vt.edu>
2022-09-05 10:01:41 -0400

    fixed dep

--- test/test_cmf.jl
+++ test/test_cmf.jl
@@ -78,4 +78,78 @@ using ActiveSpaceSolvers
     Ccmf = Cl*U
     ClusterMeanField.pyscf_write_molden(mol,Ccmf,filename="cmf.molden")
   
+    
+
+end
+    
+@testset "CMF open shell" begin
+# 
+    atoms = []
+    push!(atoms,Atom(1,"H",[0,0,0]))
+    push!(atoms,Atom(2,"H",[1,0,0]))
+    push!(atoms,Atom(3,"H",[2,0,0]))
+    push!(atoms,Atom(4,"H",[3,0,0]))
+    push!(atoms,Atom(5,"H",[4,0,0]))
+    push!(atoms,Atom(6,"H",[5,0,0]))
+    #basis = "6-31g"
+    basis = "sto-3g"
+
+    mol     = Molecule(0,1,atoms,basis)
+    mf = ClusterMeanField.pyscf_do_scf(mol)
+    nbas = size(mf.mo_coeff)[1]
+    ints = ClusterMeanField.pyscf_build_ints(mol,mf.mo_coeff, zeros(nbas,nbas));
+    e_fci, d1a_fci, d1b_fci,d2_fci = ClusterMeanField.pyscf_fci(ints,3,3)
+    # @printf(" FCI Energy: %12.8f\n", e_fci)
+
+    ClusterMeanField.pyscf_write_molden(mol,mf.mo_coeff,filename="scf.molden")
+
+    C = mf.mo_coeff
+    rdm_mf = C[:,1:2] * C[:,1:2]'
+    Cl = ClusterMeanField.localize(mf.mo_coeff,"lowdin",mf)
+    ClusterMeanField.pyscf_write_molden(mol,Cl,filename="lowdin.molden")
+    S = ClusterMeanField.get_ovlp(mf)
+    U =  C' * S * Cl
+    println(" Build Integrals")
+    flush(stdout)
+    ints = orbital_rotation(ints,U)
+    println(" done.")
+    flush(stdout)
+
+    clusters    = [(1:2),(3:4),(5:6)]
+    init_fspace = [(1,1),(1,1),(1,1)]
+
+    clusters = [Cluster(i,collect(clusters[i])) for i = 1:length(clusters)]
+    display(clusters)
+
+    rdm1 = zeros(size(ints.h1))
+    rdm1a = rdm_mf
+    rdm1b = rdm_mf
+    
+
+    e_fci = -3.155304800477
+    e_scf = -3.09169726403968
+   
+    sol = solve(ints, FCIAnsatz(6,3,3), SolverSettings())
+    display(sol)
+    
+    clusters    = [(1:2),(3:4),(5:6)]
+    init_fspace = [(1,1),(1,1),(1,1)]
+
+    clusters = [Cluster(i,collect(clusters[i])) for i = 1:length(clusters)]
+    display(clusters)
+
+    f1 = cmf_ci(ints, clusters, init_fspace, rdm1a, rdm1b, 
+                        verbose=1, sequential=false)
+    
+    @test isapprox(f1[1], -2.97293813654926351, atol=1e-10)
+    
+    e_cmf, U = cmf_oo(ints, clusters, init_fspace, rdm1, rdm1, 
+                              verbose=0, gconv=1e-6, method="cg",sequential=true)
+    @test isapprox(e_cmf, -3.205983033016, atol=1e-10)
+    
+    Ccmf = Cl*U
+    ClusterMeanField.pyscf_write_molden(mol,Ccmf,filename="cmf.molden")
+  
+    
+
 end

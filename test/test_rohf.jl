154b1fa5484ae0fe2c60ae1dcdf8c5221ed60264
Nick Mayhall <nmayhall@vt.edu>
2022-09-27 00:10:08 -0400

    cleaned up tests a bit

--- test/test_rohf.jl
+++ test/test_rohf.jl
@@ -15,10 +15,10 @@ using PyCall
     push!(atoms,Atom(2,"H",[1,0,0]))
     push!(atoms,Atom(3,"H",[2,0,0]))
     push!(atoms,Atom(4,"H",[3,0,0]))
-    #push!(atoms,Atom(5,"H",[4,0,0]))
-    #push!(atoms,Atom(6,"H",[5,0,0]))
-    #push!(atoms,Atom(7,"H",[6,0,0]))
-    #push!(atoms,Atom(8,"H",[7,0,0]))
+    push!(atoms,Atom(5,"H",[4,0,0]))
+    push!(atoms,Atom(6,"H",[5,0,0]))
+    push!(atoms,Atom(7,"H",[6,0,0]))
+    push!(atoms,Atom(8,"H",[7,0,0]))
     basis = "sto-3g"
 
     mol     = Molecule(0,3,atoms,basis)
@@ -29,38 +29,22 @@ using PyCall
     nbas = size(mf.mo_coeff)[1]
     ints = ClusterMeanField.pyscf_build_ints(mol,mf.mo_coeff, zeros(nbas,nbas));
     #ClusterMeanField.pyscf_fci(ints, 8,0)
-
+    
     clusters    = [(1:3),(4:5),(6:8)]
     init_fspace = [(3,3),(2,0),(0,0)]
     
+
     # Broken!
     clusters    = [(1:4),(5:8)]
     init_fspace = [(4,3),(1,0)]
     
     
-    clusters    = [(1,),(2,),(3,),(4,5),(6,),(7,),(8,)]
-    init_fspace = [(1,1),(1,1),(1,1),(2,0),(0,0),(0,0),(0,0)]
-    
-
-    clusters    = [(1,),(2,),(3,),(4,),(5,),(6,),(7,),(8,)]
-    init_fspace = [(1,1),(1,1),(1,1),(1,0),(1,0),(0,0),(0,0),(0,0)]
-    
-    clusters    = [(1,),(2,)]
-    init_fspace = [(1,0),(1,0)]
-    
-    clusters    = [(1,),(2,),(3,),(4,)]
-    init_fspace = [(1,1),(1,0),(0,1),(0,0)]
-    
-    
-    
     clusters = [MOCluster(i,collect(clusters[i])) for i = 1:length(clusters)]
     display(clusters)
     
-    sol = solve(ints, FCIAnsatz(4,3,1), SolverSettings())
+    sol = solve(ints, FCIAnsatz(8,5,3), SolverSettings())
     display(sol)
 
-    #display(mf.mo_coeff)
-    #display(size(mf.make_rdm1()))
     rdm_mf = mf.make_rdm1()
     rdm_mfa = rdm_mf[1,:,:]
     rdm_mfb = rdm_mf[2,:,:]
@@ -72,12 +56,8 @@ using PyCall
     #display(round.(rdm1.b, digits=7))
 
     println(" ------------------------------------------------") 
-    #f1 = ClusterMeanField.cmf_ci(ints, clusters, init_fspace, rdm1, 
-    #                    verbose=2, sequential=false)
-    f1 = ClusterMeanField.cmf_ci_iteration2(ints, clusters, rdm1, RDM2(rdm1), init_fspace, 
-                        verbose=2, sequential=false)
-    #f1 = ClusterMeanField.cmf_ci_iteration(ints, clusters, rdm1, init_fspace, 
-    #                    verbose=2, sequential=false)
+    f1 = ClusterMeanField.cmf_ci(ints, clusters, init_fspace, rdm1, 
+                        verbose=1, sequential=false)
     @printf(" ROHF Energy: %12.8f\n", mf.e_tot)
     @printf(" CMF  Energy: %12.8f\n", f1[1])
 
@@ -108,5 +88,11 @@ using PyCall
     d1, d2 = ClusterMeanField.assemble_full_rdm(clusters, tmp1, tmp2)
     e = compute_energy(ints, d1, d2)
     @printf("6RDM  Energy: %12.8f\n", e)
+    e_cmf, U, d1 = cmf_oo(ints, clusters, init_fspace, rdm1, 
+                              verbose=0, gconv=1e-6, method="cg",sequential=true)
+
+    C = mf.mo_coeff*U
+    ClusterMeanField.pyscf_write_molden(mol, mf.mo_coeff, filename="scf.molden")
+    ClusterMeanField.pyscf_write_molden(mol, C, filename="cmf.molden")
 #end
 
